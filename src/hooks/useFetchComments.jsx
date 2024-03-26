30d3e77d47e84f3be68194e4dfa0fcf781024d1a
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-03-26 15:08:18 +0100

    update

--- src/hooks/useFetchComments.jsx
+++ src/hooks/useFetchComments.jsx
@@ -5,29 +5,29 @@ const useFetchComments = (endpoint) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
 
-  const getCommentsFromApi = async () => {
-    setLoading(true);
-    try {
-      const data = await fetch(endpoint, {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWNkZGU5MGUwZWVkODAwMWEzY2FkNjEiLCJpYXQiOjE3MTE0NDg0MTgsImV4cCI6MTcxMjY1ODAxOH0.7JsncRqW6mP05TsAJBeX2OuY8bKxv-vlJStutqXRjrI",
-        },
-      });
-      const response = await data.json();
-      setData(response);
-      setLoading(false);
-    } catch (error) {
-      setError(error);
-    }
-  };
-
   useEffect(() => {
+    const getCommentsFromApi = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(endpoint, {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWNkZGU5MGUwZWVkODAwMWEzY2FkNjEiLCJpYXQiOjE3MTE0NDg0MTgsImV4cCI6MTcxMjY1ODAxOH0.7JsncRqW6mP05TsAJBeX2OuY8bKxv-vlJStutqXRjrI",
+          },
+        });
+        const jsonData = await response.json();
+        setData(jsonData);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+      }
+    };
+
     const fetchData = async () => {
       await getCommentsFromApi();
     };
+
     fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [endpoint]);
 
   return { loading, data, error };
